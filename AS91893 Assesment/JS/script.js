ce2c3e1740975f2cf29fb6e2de938a4636abe9e2
Rishiraj Singh <st22248@ormiston.school.nz>
2023-08-14 12:16:59 +1200

    Completed Navbar
    I have now successfully completed my navbar and on-top button.

--- AS91893 Assesment/JS/script.js
+++ AS91893 Assesment/JS/script.js
@@ -5,23 +5,42 @@ heroButton.addEventListener('click', function() {
   heroImage.classList.toggle('active');
 });
 
-let navbar = document.querySelector('.navbar');
-
-document.querySelector("#menu-btn").onclick= () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-}
-
-let searchForm = document.querySelector('.search-form');
-
-document.querySelector("#search-btn").onclick= () =>{
-  searchForm.classList.toggle('active');
-  navbar.classList.remove('active');
-
-}
 
 
-window.onscroll = () =>{
-  navbar.classList.remove('active');
-  searchForm.classList.remove('active');
-}
+// search bar
+
+let searchForm = document.querySelector(".search-form");
+
+// show search box
+document.querySelector("#search-btn").onclick = () => { 
+    searchForm.classList.toggle("active")
+    navbar.classList.remove("active");
+    cartItems.classList.remove("active");
+};
+
+// navBar 
+let navbar = document.querySelector(".navbar");
+
+// show navBar 
+document.querySelector("#menu-btn").onclick = () => { 
+    navbar.classList.toggle("active");
+    searchForm.classList.remove("active");
+    cartItems.classList.remove("active")
+};
+
+// target cart items container  
+let cartItems = document.querySelector(".cart-items-container"); 
+
+// show cart items
+document.querySelector("#cart-btn").onclick = () => { 
+    cartItems.classList.toggle("active")
+    searchForm.classList.remove("active");
+    navbar.classList.remove("active");
+};
+
+// remove class active on scroll window 
+window.onscroll = () => { 
+    searchForm.classList.remove("active");
+    navbar.classList.remove("active");
+    cartItems.classList.remove("active");
+};
