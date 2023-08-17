cc93bb84b2e1e1185c7803b76a4d78064c010b23
Rishiraj Singh <st22248@ormiston.school.nz>
2023-08-18 11:50:06 +1200

    Completed the draft of my home page and did some work on my chalets page
    There are quite coding to do for my chalets page but for now it is good, also added a video to make the homepage look good

--- AS91893 Assesment/JS/script.js
+++ AS91893 Assesment/JS/script.js
@@ -15,3 +15,16 @@ window.onscroll = () => {
     cartItems.classList.remove("active");
 };
 
+
+$(document).ready(function() {
+ 
+    $('.color-choose input').on('click', function() {
+        var headphonesColor = $(this).attr('data-image');
+   
+        $('.active').removeClass('active');
+        $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+        $(this).addClass('active');
+    });
+   
+  });
+  
