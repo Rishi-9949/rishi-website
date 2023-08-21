93bcf5411a229dcc707e8229ed604c86f3ecdcf6
Rishiraj Singh <st22248@ormiston.school.nz>
2023-08-21 19:46:59 +1200

    Completed Membership and a draft of chalets
    I restarted the chalets code and worked on it again.

--- AS91893 Assesment/JS/script.js
+++ AS91893 Assesment/JS/script.js
@@ -11,59 +11,6 @@ window.onscroll = () => {
     navbar.classList.remove("active");
     cartItems.classList.remove("active");
 };
-
-const imgs = document.querySelectorAll('.img-select a');
-const imgBtns = [...imgs];
-let imgId = 1;
-
-imgBtns.forEach((imgItem) => {
-    imgItem.addEventListener('click', (event) => {
-        event.preventDefault();
-        imgId = imgItem.dataset.id;
-        slideImage();
-    });
-});
-
-function slideImage(){
-    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
-
-    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
-}
-
-window.addEventListener('resize', slideImage);
-
-function submitForm() {
-    // Get the selected file name
-    const selectedFileName = document.getElementById("selectedFileName").innerHTML;
-    const card = document.getElementById("card");
-  
-    card.style.display = "block";
-  
-    // Create a new message to be displayed
-    const message = `
-      <div class="success">
-        <i class="fas fa-check-circle success-icon"></i>
-        <span>Form sent successfully!</span>
-        <br />
-      </div>
-    `;
-  
-    // Remove all the form elements from the DOM
-    const formElements = document.querySelectorAll("input, label, button");
-    for (const formElement of formElements) {
-      formElement.remove();
-    }
-  
-    // Display the success message
-    const successMessageElement = document.getElementById("successMessage");
-    successMessageElement.innerHTML = message;
-    successMessageElement.style.display = "block";
-  }
-  
-  function updateSelectedFile(inputId) {
-    const selectedFileIcon = document.getElementById(`selectedFileIcon${inputId}`);
-    selectedFileIcon.style.display = "inline-block";
-  }
   
   function resetForm() {
     const selectedFileIconProofOfID = document.getElementById("selectedFileIconproofOfID");
@@ -91,9 +38,18 @@ function submitForm() {
       selectedFileIcon.style.display = "none";
     }
   }
-  
-  
   document.getElementById("submitBtn").addEventListener("click", function() {
     window.location.href = "pages/thankyou.html"; // Relative path to your "Thank You" page
+  });
+
+  const imageSlider = document.querySelector('.image-slider');
+  const images = document.querySelectorAll('.image');
+  const thumbnails = document.querySelectorAll('.thumbnail');
+
+  thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+      images.forEach(image => image.classList.remove('active'));
+      images[index].classList.add('active');
+    });
   });
   
